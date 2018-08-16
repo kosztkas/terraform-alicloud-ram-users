c0053684d0c4a2b37b13825f314e1a17d40695c9
Sandor Kosztka <kosztkas@users.noreply.github.com>
2018-08-17 01:38:59 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,3 +3,22 @@ provider "alicloud" {
     secret_key = "${var.alicloud_secret_key}"
 }
 
+resource "alicloud_ram_user" "users" {
+  count = "${length(var.usernames)}"
+  name = "${element(var.usernames, count.index)}"
+  display_name = "${element(var.dispnames, count.index)}"
+  email = "${element(var.emails, count.index)}"
+}
+
+resource "alicloud_ram_login_profile" "profile" {
+  user_name = "${element(var.usernames, count.index)}"
+  password = "${var.password}"
+  password_reset_required = "true"
+  depends_on = ["alicloud_ram_user.users"]
+}
+
+resource "alicloud_ram_group_membership" "membership" {
+  group_name = "${var.group_name}"
+  user_names = "${var.usernames}"
+  depends_on = ["alicloud_ram_user.users"]
+}
